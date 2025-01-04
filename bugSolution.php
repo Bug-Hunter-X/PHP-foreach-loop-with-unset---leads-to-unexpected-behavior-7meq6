74e11f542d667c3f8919d6c5e73f02131671dc71
BugHunter-X <askzenai@gmail.com>
2025-01-04 02:20:07 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,17 @@
+function foo(array $arr) {
+  $keys = array_keys($arr);
+  foreach ($keys as $key) {
+    if ($arr[$key] === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+$arr = ['foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [foo] => foo [baz] => baz )
